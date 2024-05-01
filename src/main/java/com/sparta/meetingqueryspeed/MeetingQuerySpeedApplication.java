31ec0ea252b92d9e394e3be04afe7278ecae997d
OKSUchun <oksuchun119@gmail.com>
2024-05-01 20:58:49 +0900

    [Chore] Application 추가

---
+++ src/main/java/com/sparta/meetingqueryspeed/MeetingQuerySpeedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.meetingqueryspeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingQuerySpeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingQuerySpeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/meetingqueryspeed/MeetingQuerySpeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.meetingqueryspeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingQuerySpeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
